7fb3a2d15b0bdb20b27047269a3186e0f22c32e9
MxDot <masonfurness28@gmail.com>
2026-05-31 15:18:11 +0100

    Initialize Spring Boot project with Gradle configuration, build scripts, and basic application setup.

---
+++ src/main/java/me/mxdot/cti/CtiApplication.java
@@ -0,0 +1,13 @@
+package me.mxdot.cti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mxdot/cti/CtiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mxdot.cti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
